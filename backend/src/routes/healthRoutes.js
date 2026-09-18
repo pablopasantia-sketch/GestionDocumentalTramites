33fa0488573ff8caaeee17fb1a341493e273e002
Pablo Jancko <jancko29@gmail.com>
2026-09-18 11:43:57 -0400

    chore: remove backend application codebase

--- backend/src/routes/healthRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { getHealth } = require('../controllers/healthController');
-
-const router = Router();
-
-// Endpoint público para chequear salud de API y Base de Datos MySQL 8.0
-router.get('/', getHealth);
-
-module.exports = router;
